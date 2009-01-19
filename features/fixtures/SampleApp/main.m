32689d9d5a8018b9f600d10275770d81d58de5bd
Dr Nic Williams <drnicwilliams@gmail.com>
2009-01-19 15:49:31 +1000

    replaced fixture SampleApp with pure Cocoa app instead of MacRuby app

--- features/fixtures/SampleApp/main.m
+++ features/fixtures/SampleApp/main.m
@@ -6,9 +6,9 @@
 //  Copyright Mocra 2009. All rights reserved.
 //
 
-#import <MacRuby/MacRuby.h>
+#import <Cocoa/Cocoa.h>
 
 int main(int argc, char *argv[])
 {
-    return macruby_main("rb_main.rb", argc, argv);
+    return NSApplicationMain(argc,  (const char **) argv);
 }
