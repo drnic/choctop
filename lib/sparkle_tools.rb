90e73f9120e8b392de3f7a85ad7d504867068320
Dr Nic Williams <drnicwilliams@gmail.com>
2009-01-19 13:24:13 +1000

    using a DSL instead of constants

--- lib/sparkle_tools.rb
+++ lib/sparkle_tools.rb
@@ -1,13 +1,48 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
-module SparkleTools
+class SparkleTools
   VERSION = '0.0.1'
+  
+  attr_accessor :name
+  attr_accessor :server
+  attr_accessor :base_url
+  attr_accessor :appcast_filename
+  attr_accessor :remote_dir
+  attr_accessor :rsync_args
+  
+  def initialize(name)
+    @name = name
+    
+    # Defaults
+    @appcast_filename = 'linker_appcast.xml'
+    @rsync_args = '-av'
+    
+    yield self if block_given?
+
+    @base_url ||= "http://#{server}"
+    
+    define_tasks
+  end
+
+  def define_tasks
+    namespace :appcast do
+      desc "Create/update the appcast file"
+      task :build do
+        make_appcast
+      end
+
+      desc "Upload the appcast file to the server"
+      task :upload do
+        upload_appcast
+      end
+    end
+  end
 end
+require "sparkle_tools/appcast"
 
 require "fileutils"
 require "builder"
-require "sparkle_tools/appcast"
 
 require "rubygems"
 require "active_support"
--- lib/sparkle_tools/appcast.rb
+++ lib/sparkle_tools/appcast.rb
@@ -9,7 +9,7 @@ module SparkleTools::Appcast
     app_name = File.basename(File.expand_path('.'))
     
     FileUtils.mkdir_p "appcast/build"
-    appcast = File.open("appcast/build/#{APPCAST_FILENAME}", 'w')
+    appcast = File.open("appcast/build/#{appcast_filename}", 'w')
 
     xml = Builder::XmlMarkup.new(:target => appcast, :indent => 2)
 
@@ -20,11 +20,11 @@ module SparkleTools::Appcast
       xml.channel do
         xml.title(app_name)
         xml.description('#{app_name} updates')
-        xml.link(APPCAST_URL)
+        xml.link(base_url)
         xml.language('en')
         xml.pubDate Time.now.to_s(:rfc822)
         # xml.lastBuildDate(Time.now.rfc822)
-        xml.atom(:link, :href => "#{APPCAST_URL}/#{APPCAST_FILENAME}", 
+        xml.atom(:link, :href => "#{base_url}/#{appcast_filename}", 
                  :rel => "self", :type => "application/rss+xml")
 
         versions.each do |version|
@@ -36,7 +36,7 @@ module SparkleTools::Appcast
             xml.title(items['title'])
             xml.description(items['description'])
             xml.pubDate(File.mtime(file))
-            xml.enclosure(:url => "#{APPCAST_URL}/#{items['filename']}", 
+            xml.enclosure(:url => "#{base_url}/#{items['filename']}", 
                           :length => "#{File.size(file)}", :type => "application/zip")
             xml.guid(guid, :isPermaLink => "false")
           end
@@ -46,41 +46,7 @@ module SparkleTools::Appcast
   end
 
   def upload_appcast
-    remote_dir = APPCAST_REMOTE_DIR
-
-    Net::SSH.start( APPCAST_SERVER, 'deploy' ) do |session|
-      cwd = Dir.pwd
-      Dir.chdir('appcast/build')
-
-      shell = session.shell.sync
-
-      begin
-        out = shell.cd remote_dir
-        raise "Failed to change to proper remote directory." unless out.status == 0  
-
-        out = shell.ls("-1")
-        raise "Failed to get directory listing." unless out.status == 0
-
-        files = Array.new
-        out.stdout.each { |file| files << file.strip }
-
-        # Look through the list of files and see what we need to upload, as 
-        # compared to what we have locally - but always upload the appcast itself
-        local_files = Dir.glob('*')
-        files.delete(APPCAST_FILENAME)  # we always upload this
-        local_files.each do |local_file|
-          unless files.include?(local_file)
-            print "Uploading: #{local_file}... "
-            `scp #{local_file} deploy@#{APPCAST_SERVER}:#{remote_dir}`
-            puts $?.exitstatus == 0 ? "done." : "FAILED!"
-          end
-        end
-      rescue => e
-        puts "Failed: #{e.message}"
-      ensure
-        Dir.chdir(cwd)
-        shell.exit  
-      end
-    end
+    # rsync_args = '-av --delete --ignore-errors'
   end
 end
+SparkleTools.send(:include, SparkleTools::Appcast)
