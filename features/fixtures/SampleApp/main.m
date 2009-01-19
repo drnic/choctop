19005d447886063beec56d805b911685b49dde50
Dr Nic Williams <drnicwilliams@gmail.com>
2009-01-19 15:43:20 +1000

    SampleApp for scenarios

---
+++ features/fixtures/SampleApp/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  SampleApp
+//
+//  Created by Dr Nic on 19/01/09.
+//  Copyright Mocra 2009. All rights reserved.
+//
+
+#import <MacRuby/MacRuby.h>
+
+int main(int argc, char *argv[])
+{
+    return macruby_main("rb_main.rb", argc, argv);
+}
