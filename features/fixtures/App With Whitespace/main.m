4623b4f09c47597ea59a7bb849ef2775db991e04
Dr Nic Williams <drnicwilliams@gmail.com>
2009-02-03 23:50:36 +1000

    missing 'App With Whitespace' for scenarios

---
+++ features/fixtures/App With Whitespace/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  App With Whitespace
+//
+//  Created by Dr Nic on 3/02/09.
+//  Copyright Mocra 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
