e821c2d32119096a053a8d11999575610b0fc670
Dr Nic Williams <drnicwilliams@gmail.com>
2009-06-10 22:26:42 +1000

    finished upgrading cucumber features to latest newgem install_cucumber files + structure

--- features/support/common.rb
+++ features/support/common.rb
@@ -22,6 +22,7 @@ module CommonHelpers
 
   def setup_active_project_folder project_name
     @active_project_folder = File.join(@tmp_root, project_name)
+    FileUtils.mkdir_p(@active_project_folder)
     @project_name = project_name
   end
 
