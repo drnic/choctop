6e03b65bf602269fccc1672fc7bf0196b167bf3c
Billy Gray <wgray@zetetic.net>
2010-04-15 16:35:06 -0400

    bug fix for rake:feed, 'undefined method `rfc822'', just use strftime, no time to screw with rails dependencies, for the love of god.

--- lib/choctop/appcast.rb
+++ lib/choctop/appcast.rb
@@ -21,7 +21,7 @@ module ChocTop::Appcast
           xml.description("#{app_name} updates")
           xml.link(base_url)
           xml.language('en')
-          xml.pubDate Time.now.to_s(:rfc822)
+          xml.pubDate( Time.now.strftime("%a, %d %b %Y %H:%M:%S %z") )
           # xml.lastBuildDate(Time.now.rfc822)
           xml.atom(:link, :href => "#{base_url}/#{appcast_filename}", 
                    :rel => "self", :type => "application/rss+xml")
@@ -29,7 +29,7 @@ module ChocTop::Appcast
           xml.item do
             xml.title("#{name} #{version}")
             xml.tag! "sparkle:releaseNotesLink", "#{base_url}/#{release_notes}"
-            xml.pubDate Time.now.to_s(:rfc822) #(File.mtime(pkg))
+            xml.pubDate Time.now.strftime("%a, %d %b %Y %H:%M:%S %z")
             xml.guid("#{name}-#{version}", :isPermaLink => "false")
             xml.enclosure(:url => "#{base_url}/#{pkg_name}", 
                           :length => "#{File.size(pkg)}", 
