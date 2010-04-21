bbec848f6801e79f8eb8ab9fb08e65755dca5c49
Patrick Huesler <patrick.huesler@gmail.com>
2010-04-21 16:41:57 +0200

    Moved rake tasks into their own module

--- lib/choctop.rb
+++ lib/choctop.rb
@@ -10,10 +10,12 @@ require "RedCloth"
 require 'choctop/appcast'
 require 'choctop/dmg'
 require 'choctop/version_helper'
+require 'choctop/rake_tasks'
 
 class ChocTop
   include Appcast
   include Dmg
+  include RakeTasks
   
   VERSION = '0.11.0'
   
@@ -273,76 +275,5 @@ class ChocTop
     add_file :target_bundle, :position => app_icon_position
     
     define_tasks
-  end
-  
-  def define_tasks
-    return unless Object.const_defined?("Rake")
-    
-    desc "Build Xcode #{build_type}"
-    task :build => "build/#{build_type}/#{target}/Contents/Info.plist"
-    
-    task "build/#{build_type}/#{target}/Contents/Info.plist" do
-      make_build
-    end
-    
-    desc "Create the dmg file for appcasting"
-    task :dmg => :build do
-      detach_dmg
-      make_dmg
-      detach_dmg
-      convert_dmg_readonly
-      add_eula
-    end
-    
-    desc "Create/update the appcast file"
-    task :feed do
-      make_appcast
-      make_dmg_symlink
-      make_index_redirect
-      make_release_notes
-    end
-    
-    desc "Upload the appcast file to the host"
-    task :upload => :feed do
-      upload_appcast
-    end
-
-    task :detach_dmg do
-      detach_dmg
-    end
-    
-    task :size do
-      puts configure_dmg_window
-    end
-    
-    namespace :version do
-      desc "Display the current version"
-      task :current do
-        puts VersionHelper.new(info_plist_path).to_s
-      end
-
-      namespace :bump do
-        desc "Bump the gemspec by a major version."
-        task :major do
-          VersionHelper.new(info_plist_path) do |version|
-            version.bump_major
-          end
-        end
-
-        desc "Bump the gemspec by a minor version."
-        task :minor do
-          VersionHelper.new(info_plist_path) do |version|
-            version.bump_minor
-          end
-        end
-
-        desc "Bump the gemspec by a patch version."
-        task :patch do
-          VersionHelper.new(info_plist_path) do |version|
-            version.bump_patch
-          end
-        end
-      end
-    end
   end
 end
---
+++ lib/choctop/rake_tasks.rb
@@ -0,0 +1,72 @@
+module RakeTasks
+  def define_tasks
+    return unless Object.const_defined?("Rake")
+
+    desc "Build Xcode #{build_type}"
+    task :build => "build/#{build_type}/#{target}/Contents/Info.plist"
+
+    task "build/#{build_type}/#{target}/Contents/Info.plist" do
+      make_build
+    end
+
+    desc "Create the dmg file for appcasting"
+    task :dmg => :build do
+      detach_dmg
+      make_dmg
+      detach_dmg
+      convert_dmg_readonly
+      add_eula
+    end
+
+    desc "Create/update the appcast file"
+    task :feed do
+      make_appcast
+      make_dmg_symlink
+      make_index_redirect
+      make_release_notes
+    end
+
+    desc "Upload the appcast file to the host"
+    task :upload => :feed do
+      upload_appcast
+    end
+
+    task :detach_dmg do
+      detach_dmg
+    end
+
+    task :size do
+      puts configure_dmg_window
+    end
+
+    namespace :version do
+      desc "Display the current version"
+      task :current do
+        puts VersionHelper.new(info_plist_path).to_s
+      end
+
+      namespace :bump do
+        desc "Bump the gemspec by a major version."
+        task :major do
+          VersionHelper.new(info_plist_path) do |version|
+            version.bump_major
+          end
+        end
+
+        desc "Bump the gemspec by a minor version."
+        task :minor do
+          VersionHelper.new(info_plist_path) do |version|
+            version.bump_minor
+          end
+        end
+
+        desc "Bump the gemspec by a patch version."
+        task :patch do
+          VersionHelper.new(info_plist_path) do |version|
+            version.bump_patch
+          end
+        end
+      end
+    end
+  end
+end
